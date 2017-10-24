d3fa5f965e749ce9e812641974d2733300541e51
MinKKKKKK <343733617@qq.com>
2017-10-24 23:36:09 +0800

    Update 2017-10-24-23:36 Update 2017-10-24-23:36

--- m_ArrayList.h
+++ m_ArrayList.h
@@ -17,6 +17,7 @@ typedef struct Arr {
 
 #pragma region Functions
 Arr* ArrInit(int size);
+Arr* ArrInit_InputKeyCode();
 Status IsEmpty(Arr* arr);
 void ShowArr(Arr* arr);
 Status IsFull(Arr* arr);
@@ -49,6 +50,34 @@ Arr* ArrInit(int size)
 	return ret;								//返回之后局部指针变量消失，但是地址里面的value还在
 }
 
+//Arr手动初始化
+Arr* ArrInit_InputKeyCode()
+{
+	Arr* ret = (Arr*)malloc(sizeof(Arr));
+	if (NULL == ret) {
+		printf("Arr动态分配内存失败！\n");
+		exit(-1);
+		return NULL;
+	}
+
+	int size;
+	printf("请输入初始化数组的元素数目Count :");
+	scanf_s("%d", &size);
+
+	ret->Size = size * 2;	//最大长度设置为2倍
+	ret->Count = 0;
+	ret->pBase = (int *)malloc(sizeof(int) * ret->Size);
+	ret->Increment = 2;						//一开始增长因子为2倍，2倍速度扩展数组长度
+	for (int i = 0; i < size; i++)
+	{
+		printf("初始化第[%d]个元素的value为 :",i);
+		int arg;
+		scanf_s("%d", &arg);
+		Append(ret, arg);
+	}
+	return ret;
+}
+
 //Arr全部打印
 void ShowArr(Arr* arr)
 {
@@ -68,6 +97,11 @@ void ShowArr(Arr* arr)
 //Arr是否为空
 Status IsEmpty(Arr* arr)
 {
+	if(arr == NULL){
+		printf("参数为NULL\n");
+		exit(-1);
+	}
+
 	if (arr->Count == 0) return TRUE;
 	else return FALSE;
 }
@@ -75,6 +109,11 @@ Status IsEmpty(Arr* arr)
 //Arr是否为满
 Status IsFull(Arr* arr)
 {
+	if(arr == NULL){
+		printf("参数为NULL\n");
+		exit(-1);
+	}
+
 	if (arr->Count == arr->Size) return TRUE;
 	else return FALSE;
 }
@@ -103,18 +142,17 @@ void Insert(Arr* arr,int index,int value)
 		return;
 	}
 	if (arr->Count <= index || index < 0) {
-		printf("Please input a right index!\n");
+		printf("无法访问第[%d]个位置的value\n",index);
 		return;
 	}
-	else{
-		for (int i = arr->Count - 1; i >= index; i--){
-			arr->pBase[i + 1] = arr->pBase[i];
-		}
-		arr->Count++;
-		arr->pBase[index] = value;
-		//动态扩充数组
-		if (IsBeingFull(arr)) Allocate(arr);
+
+	for (int i = arr->Count - 1; i >= index; i--){
+		arr->pBase[i + 1] = arr->pBase[i];
 	}
+	arr->Count++;
+	arr->pBase[index] = value;
+	//动态扩充数组
+	if (IsBeingFull(arr)) Allocate(arr);
 	return;
 }
 
@@ -146,7 +184,7 @@ int Get(Arr* arr,int index)
 		exit(-1);
 	}
 	if (arr->Count <= index || index < 0) {
-		printf("Please input a right index!\n");
+		printf("无法获取第[%d]个位置的value\n",index);
 		exit(-1);
 	}
 	else {
@@ -172,7 +210,13 @@ void Inversion(Arr* arr)
 }
 
 //Arr 将要满了的时候
-Status IsBeingFull(Arr* arr) {
+Status IsBeingFull(Arr* arr)
+{
+	if(arr == NULL){
+		printf("参数为NULL\n");
+		return;
+	}
+
 	if (arr->Count + 3 == arr->Size) return TRUE;
 	else return FALSE;
 }
@@ -180,6 +224,11 @@ Status IsBeingFull(Arr* arr) {
 //Arr长度动态扩展
 void Allocate(Arr* arr)
 {
+	if(arr == NULL){
+		printf("参数为NULL\n");
+		return;
+	}
+
 	if (arr->Size >= 4 * 1000)    arr->Increment = 1.25;	//1.25倍的速度
 
 	int* ret = (int*)malloc(sizeof(int)* arr->Size * arr->Increment);
@@ -204,7 +253,7 @@ int GetSize(Arr* arr)
 {
 	if (arr == NULL) {
 		printf("请检查参数是否为NULL\n");
-		return -1;
+		exit(-1);
 	}
 	return arr->Count;
 }
@@ -212,7 +261,12 @@ int GetSize(Arr* arr)
 //Arr_BubbleSort
 void Arr_BubbleSort(Arr* arr)
 {
-	for (int i = 0; i < arr->Count; i++)
+	if(arr == NULL){
+		printf("参数为NULL\n");
+		return;
+	}
+
+	for (int i = 0; i < arr->Count - 1; ++i)
 	{
 		for (int j = i+1; j < arr->Count; ++j)
 		{
@@ -230,6 +284,11 @@ void Arr_BubbleSort(Arr* arr)
 //Arr InsertSort
 void Arr_InsetSort(Arr* arr)
 {
+	if(arr == NULL){
+		printf("参数为NULL\n");
+		return;
+	}
+
 	for (int i = 0; i < arr->Count - 1; i++)
 	{
 		int index = i + 1;
