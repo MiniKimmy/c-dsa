653e13da7ad2aafd69f56341f135ceb82ffcdd30
MinKKKKKK <343733617@qq.com>
2017-11-11 22:55:54 +0800

    改善了了Init LinkList

--- m_List/m_LinkList.h
+++ m_List/m_LinkList.h
@@ -1,16 +1,17 @@
 #pragma once
 #include<stdio.h>
 #include<stdlib.h>
+#define PRINT_STRING(x) printf("%s\n",x)
 
-#pragma region Status
-#define FALSE 0
-#define TRUE 1
-typedef int Status;
-#pragma endregion
+typedef enum Status{
+    FALSE,
+    TRUE,
+}Status;
+
+typedef int Element;    //Include到其他文件要修改该Element类型
 
 #pragma region LinkList
-/*双哨兵+双向链表 --（查询路径缩短一半）*/
-typedef int Element;
+/*双哨兵+双向链表*/
 typedef struct Node {
     Element Data;
     struct Node * Next;
@@ -18,79 +19,130 @@ typedef struct Node {
 }Node;
 
 typedef struct LinkList {
-    struct Node * Head;    //头哨兵
-    struct Node * Tail;    //尾哨兵
-    int Count;    //链表长度
+    struct Node * Head;
+    struct Node * Tail;
+    int Count;
 }LinkList;
 #pragma endregion
 
 #pragma region Functions;
-LinkList* LinkListInit_InputArr(int* arr, int size);
-Status IsEmpty(LinkList * list);
-LinkList* LinkListInit_KeyCodeInput();
-void ShowLinkList(LinkList * list);
-void Insert(LinkList * list, int index, Element value);
-void Append(LinkList* list, Element value);
-void Delete(LinkList * list, int index);
-void Remove(LinkList * list);
-Element Get(LinkList * list, int index);
-Element GetFirst(LinkList * list);
-void SetValue(LinkList * list, int index, Element value);
-void Destroy(LinkList** list);
-void Clear(LinkList * list);
-void Inversion(LinkList * list);
-void LinkList_BubbleSort(LinkList * list);
-void LinkList_InsertSort(LinkList * list);
-int GetSize(LinkList * list);
+LinkList* InitLinkList();
+void CreateLinkList_ByKeyCodeInput(LinkList * L);
+void CreateLinkList_ByInputArray(LinkList * L, int* arr, int size);
+Status IsEmptyLinkList(LinkList * L);
+void Append_LinkList(LinkList * L, Element value);
+void Insert_LinkList(LinkList * L, int index, Element value);
+void Traverse_LinkList(LinkList * L);
+void DeleteNode_LinkList(LinkList * L, int index);
+void Remove_LinkList(LinkList * L);
+Element GetValue_LinkList(LinkList * L, int index);
+Element GetFirst_LinkList(LinkList * L);
+void SetValue_LinkList(LinkList * L, int index, Element value);
+void Destroy_LinkList(LinkList** L);
+void Clear_LinkList(LinkList * L);
+void Inversion_LinkList(LinkList * L);
+void LinkList_BubbleSort(LinkList * L);
+void LinkList_InsertSort(LinkList * L);
+int GetSize_LinkList(LinkList * L);
 #pragma endregion
 
+/*初始化LinkList*/
+LinkList* InitLinkList()
+{
+    Node* m_Head = (Node*)malloc(sizeof(Node));
+    if (m_Head == NULL) {
+        PRINT_STRING("Node初始化动态分配内存失败");
+        exit(-1);
+    }
 
+    LinkList* ret = (LinkList*)malloc(sizeof(LinkList));
+    if (ret == NULL) {
+        printf("LinkList动态分配内存失败\n");
+        exit(-1);
+    }
+    m_Head->Next = NULL;
+    m_Head->Front = NULL;
+    ret->Head = m_Head;
+    ret->Tail = m_Head;
+    ret->Count = 0;
+    return ret;
+}
+
+//手动输入初始化LinkList
+void CreateLinkList_ByKeyCodeInput(LinkList * L)
+{
+    PRINT_STRING("请输入需要初始化元素的数量 :");
+    int size;
+    scanf_s("%d", &size);
+    while (size < 0){
+        PRINT_STRING("size不能为负值,请重新输入size的值：");
+        scanf_s("%d", &size);
+    }
+
+    for (int i = 0; i < size; i++) {
+        printf("请输入第%d个元素的value :", i);
+        Element arg;
+        scanf_s("%d", &arg);
+        Append_LinkList(L, arg);
+    }
+    return;
+}
+
+//判断LinkList是否为空
+Status IsEmptyLinkList(LinkList * L)
+{
+    if (L == NULL) {
+        PRINT_STRING("List为NULL\n");
+        exit(-1);
+    }
+
+    if (L->Count == 0) return TRUE;
+    else return FALSE;
+
+
+}
 
 //LinkList追加
-void Append(LinkList * list,Element value)
+/*在LinkList的最后面添加新元素*/
+void Append_LinkList(LinkList * L, Element value)
 {
-    if (list == NULL) {
-        printf("空指针无效\n");
+    if (L == NULL) {
+        PRINT_STRING("List为NULL");
         return;
     }
+
     Node* p = (Node*)malloc(sizeof(Node));
     p->Data = value;
     p->Next = NULL;
-    p->Front = list->Tail;
-    list->Tail->Next = p;
-    list->Tail = p;
-    list->Count++;
+    p->Front = L->Tail;
+    L->Tail->Next = p;
+    L->Tail = p;
+    L->Count++;
     return;
 }
 
 //LinkList插入
-/*index：插入的位置，位置下标大于链表长度自动添加在链表最后*/
-void Insert(LinkList * list, int index, Element value)
+/*index：插入新元素的位置*/
+void Insert_LinkList(LinkList * L, int index, Element value)
 {
-    if (list == NULL) {
-        printf("空指针无效\n");
+    if (L == NULL) {
+        PRINT_STRING("L为NULL");
         return;
     }
 
-    if (index < 0) {
-        printf("index不可以为负数\n");
-        return;
-    }
-
-    if (index >= list->Count){
-        Append(list, value);
+    if (index < 0 || index >= L->Count) {
+        printf("无法在第[%d]的位置插入元素\n", index);
         return;
     }
 
     Node* p;
-    if (index > list->Count / 2) {
-        p = list->Tail;
-        for (int i = list->Count - 1; i >= index; i--){
+    if (index > L->Count / 2) {
+        p = L->Tail;
+        for (int i = L->Count - 1; i >= index; i--) {
             p = p->Front;
         }
-    }
-    else {
-        p = list->Head;
+    }else {
+        p = L->Head;
         for (int i = 0; i < index; i++) {
             p = p->Next;
         }
@@ -102,141 +154,56 @@ void Insert(LinkList * list, int index, Element value)
     item->Front = p;
     p->Next->Front = item;
     p->Next = item;
-    list->Count++;
+    L->Count++;
     return;
 }
 
-//手动输入初始化LinkList
-LinkList* LinkListInit_KeyCodeInput()
+//通过数组赋值存储int类型Element的LinkList
+void CreateLinkList_ByInputArray(LinkList * L, int* arr, int size)
 {
-    printf("请输入需要初始化元素的数量 :");
-    int size;
-    scanf_s("%d", &size);
-    if (size < 0 ) {
-        printf("输入数量出错\n");
-        exit(-1);
-    }
-
-    Node* m_Head = (Node*)malloc(sizeof(Node));
-    if (m_Head == NULL) {
-        printf("头结点动态分配内存失败\n");
-        exit(-1);
-    }
-    LinkList* ret = (LinkList*)malloc(sizeof(LinkList));
-    if (ret == NULL) {
-        printf("LinkList动态分配内存失败\n");
-        exit(-1);
-    }
-
-    //初始化结构体成员
-    m_Head->Next = NULL;
-    m_Head->Front = NULL;
-    ret->Head = m_Head;
-    ret->Tail = m_Head;
-    ret->Count = 0;
-
-    //为链表添加数据
-    for (int i = 0; i < size; i++)
-    {
-        printf("请输入第%d个元素的value :", i);
-        int arg;
-        scanf_s("%d", &arg);
-        Append(ret, arg);
-    }
-    return ret;
-}
-
-//通过数组初始化LinkList
-LinkList* LinkListInit_InputArr(int* arr,int size)
-{
-    if (arr == NULL) {
-        printf("传入的int数组为空\n");
-        exit(-1);
-    }
-    if (size < 0) {
-        printf("Size不能为负数\n");
-        exit(-1);
-    }
-
-    Node* m_Head = (Node*)malloc(sizeof(Node));
-    if (m_Head == NULL) {
-        printf("头结点动态分配内存失败\n");
-        exit(-1);
-    }
-    LinkList* ret = (LinkList*)malloc(sizeof(LinkList));
-    if (ret == NULL) {
-        printf("LinkList动态分配内存失败\n");
-        exit(-1);
-    }
-
-    //初始化结构体成员
-    m_Head->Next = NULL;
-    m_Head->Front = NULL;
-    ret->Count = 0;
-    ret->Head = m_Head;
-    ret->Tail = m_Head;
-
-    //写入具体数据
-    for (int i = 0; i < size; i++){
-        Append(ret, arr[i]);
-
-    }
-    return ret;
-}
-
-//判断是否为空
-Status IsEmpty(LinkList * list)
-{
-    if (list == NULL) {
-        printf("空指针无效\n");
-        exit(-1);
-    }
-
-    if (list->Count == 0) return TRUE;
-    else return FALSE;
-
-    /*if (list->Head == list->Tail) return TRUE;
-    else return FALSE;*/
-}
-
-//遍历LinkList
-void ShowLinkList(LinkList * list)
-{
-    if (IsEmpty(list)) {
-        printf("空LinkList,无元素\n");
+    if (arr == NULL || L == NULL) {
+        PRINT_STRING("传入参数有NULL");
         return;
     }
 
-    Node* p = list->Head->Next;
-    while (p){
-        printf("%d ", p->Data);
+    for (int i = 0; i < size; i++) {
+        Append_LinkList(L, arr[i]);
+    }
+    return;
+}
+
+//遍历LinkList
+void Traverse_LinkList(LinkList * L)
+{
+    if (IsEmptyLinkList(L)) return;
+
+    Node* p = L->Head->Next;
+    while (p) {
+        printf("%d\n", p->Data);
         p = p->Next;
     }
-    printf("\n");
     return;
 }
 
 //获取指定index的value
-Element Get(LinkList * list, int index)
+Element GetValue_LinkList(LinkList * L, int index)
 {
-    if (IsEmpty(list)) {
-        printf("空LinkList,无元素\n");
-        return NULL;
-    }
-    if (index < 0 || index >= list->Count){
+    if (IsEmptyLinkList(L)) return NULL;
+    
+    if (index < 0 || index >= L->Count) {
         printf("无法获取第[%d]个位置的元素\n", index);
         return NULL;
     }
 
-    Node* p ;
-    if (index > list->Count / 2) {
-        p = list->Tail;
-        for (int i = list->Count - 1; i > index; i--){
+    Node* p;
+    if (index > L->Count / 2) {
+        p = L->Tail;
+        for (int i = L->Count - 1; i > index; i--) {
             p = p->Front;
         }
     }
-    else{
-        p = list->Head;
+    else {
+        p = L->Head;
         for (int i = 0; i <= index; i++) {
             p = p->Next;
         }
@@ -245,65 +212,56 @@ Element Get(LinkList * list, int index)
 }
 
 //获取第一个元素的value
-Element GetFirst(LinkList * list)
+Element GetFirst_LinkList(LinkList * L)
 {
-    if (IsEmpty(list)) {
-        printf("空LinkList,无元素\n");
-        return NULL;
-    }
-
-    return list->Head->Next->Data;
+    if (IsEmptyLinkList(L)) return NULL;
+    return L->Head->Next->Data;
 }
 
 //修改指定index的value
-void SetValue(LinkList * list, int index, Element value)
+void SetValue_LinkList(LinkList * L, int index, Element value)
 {
-    if (IsEmpty(list)) {
-        printf("空LinkList,无元素\n");
-        return;
-    }
-    if (index < 0 || index >= list->Count) {
+    if (IsEmptyLinkList(L)) return;
+    if (index < 0 || index >= L->Count) {
         printf("无法修改第[%d]个位置的元素value\n", index);
         return;
     }
 
     Node* p;
-    if (index > list->Count / 2) {
-        p = list->Tail;
-        for (int i = list->Count - 1; i > index; i--) {
+    if (index > L->Count / 2) {
+        p = L->Tail;
+        for (int i = L->Count - 1; i > index; i--) {
             p = p->Front;
         }
     }
     else {
-        p = list->Head;
+        p = L->Head;
         for (int i = 0; i <= index; i++) {
             p = p->Next;
         }
     }
     p->Data = value;
+    return;
 }
 
 //删除指定index的value
-void Delete(LinkList * list, int index)
+void DeleteNode_LinkList(LinkList * L, int index)
 {
-    if (IsEmpty(list)) {
-        printf("空LinkList,无元素\n");
-        return;
-    }
-    if (index < 0 || index >= list->Count) {
+    if (IsEmptyLinkList(L)) return;
+    
+    if (index < 0 || index >= L->Count) {
         printf("无法删除第[%d]个位置的元素\n", index);
         return;
     }
 
     Node* p;
-    if (index > list->Count / 2) {
-        p = list->Tail;
-        for (int i = list->Count - 1; i >= index; i--) {
+    if (index > L->Count / 2) {
+        p = L->Tail;
+        for (int i = L->Count - 1; i >= index; i--) {
             p = p->Front;
         }
-    }
-    else {
-        p = list->Head;
+    }else {
+        p = L->Head;
         for (int i = 0; i < index; i++) {
             p = p->Next;
         }
@@ -315,32 +273,50 @@ void Delete(LinkList * list, int index)
     free(temp);
     temp->Next = NULL;
     temp->Front = NULL;
-    list->Count--;
+    L->Count--;
     return;
 }
 
 //删除最后一个value
-void Remove(LinkList * list)
+void Remove_LinkList(LinkList * L)
 {
-    if (IsEmpty(list)) {
-        printf("空LinkList,无元素\n");
-        return;
-    }
-
-    Node * p = list->Tail;
-    list->Tail = p->Front;
+    if (IsEmptyLinkList(L)) return;
+    
+    Node * p = L->Tail;
+    L->Tail = p->Front;
     free(p);
     p->Front = NULL;
-    list->Tail->Next = NULL;
-    list->Count--;
+    L->Tail->Next = NULL;
+    L->Count--;
     return;
 }
 
-//销毁LinkList
-/*freeLinkList,返回NULL*/
-void Destroy(LinkList** list)
+/*清除LinkList ,返回一个空元素的LinkList*/
+void Clear_LinkList(LinkList * L)
 {
-    Node* p = (*list)->Head;
+    if (IsEmptyLinkList(L)) return;
+
+    Node* p = L->Head->Next;
+    while (p) {
+        Node* q = p->Next;
+        free(p);
+        p->Front = NULL;
+        p->Next = NULL;
+        p = q;
+    }
+    L->Tail = L->Head;
+    L->Head->Next = NULL;
+    L->Head->Front = NULL;
+    L->Count = 0;
+    return;
+}
+
+/*销毁LinkList,返回NULL*/
+void Destroy_LinkList(LinkList** L)
+{
+    if (*L == NULL) return;
+
+    Node* p = (*L)->Head;
     while (p) {
         Node* q = p->Next;
         free(p);
@@ -349,48 +325,21 @@ void Destroy(LinkList** list)
         p = q;
     }
 
-    free(*list);
-    (*list)->Tail = NULL;
-    (*list)->Head = NULL;
-    *list = NULL;
-    return;
-}
-
-//清除LinkList
-/*返回一个空元素的LinkList*/
-void Clear(LinkList * list)
-{
-    if (IsEmpty(list)) {
-        printf("空序列无法排序\n");
-        return;
-    }
-
-    Node* p = list->Head->Next;
-    while (p) {
-        Node* q = p->Next;
-        free(p);
-        p->Front = NULL;
-        p->Next = NULL;
-        p = q;
-    }
-    list->Tail = list->Head;
-    list->Head->Next = NULL;
-    list->Head->Front = NULL;
-    list->Count = 0;
+    free(*L);
+    (*L)->Tail = NULL;
+    (*L)->Head = NULL;
+    *L = NULL;
     return;
 }
 
 //LinkList倒置
-void Inversion(LinkList * list)
+void Inversion_LinkList(LinkList * L)
 {
-    if (IsEmpty(list)) {
-        printf("空序列无法Inversion操作\n");
-        return;
-    }
-
-    Node* p = list->Head->Next;
-    Node* q = list->Tail;
-    for (int i = 0; i < list->Count / 2; i++)
+    if (IsEmptyLinkList(L)) return;
+    
+    Node* p = L->Head->Next;
+    Node* q = L->Tail;
+    for (int i = 0; i < L->Count / 2; i++)
     {
         int temp = p->Data;
         p->Data = q->Data;
@@ -402,18 +351,15 @@ void Inversion(LinkList * list)
 }
 
 //LinkList冒泡排序
-void LinkList_BubbleSort(LinkList * list)
+void LinkList_BubbleSort(LinkList * L)
 {
-    if (IsEmpty(list)) {
-        printf("空序列无法排序\n");
-        return;
-    }
+    if (IsEmptyLinkList(L)) return;
 
     int i, j;
     Node* p;
     Node* q;
-    for (i = 0, p = list->Head->Next; i < list->Count - 1; i++,p = p->Next){
-        for (j = i+1,q = p->Next; j < list->Count;j++,q = q->Next){
+    for (i = 0, p = L->Head->Next; i < L->Count - 1; i++, p = p->Next) {
+        for (j = i + 1, q = p->Next; j < L->Count; j++, q = q->Next) {
             if (p->Data > q->Data) {
                 int temp = p->Data;
                 p->Data = q->Data;
@@ -425,22 +371,19 @@ void LinkList_BubbleSort(LinkList * list)
 }
 
 //LinkList插入排序
-void LinkList_InsertSort(LinkList * list)
+void LinkList_InsertSort(LinkList * L)
 {
-    if (IsEmpty(list)) {
-        printf("空序列无法排序\n");
-        return;
-    }
+    if (IsEmptyLinkList(L)) return;
 
     int i;
     Node* p;
-    for (i = 0,p = list->Head->Next; i < list->Count - 1; i++,p = p->Next)
+    for (i = 0, p = L->Head->Next; i < L->Count - 1; i++, p = p->Next)
     {
         Node* q = p->Next;
         Node* t = p;
         while (q)
         {
-            if (t->Data > q->Data){
+            if (t->Data > q->Data) {
                 t = q;
                 q = q->Next;
             }
@@ -448,7 +391,7 @@ void LinkList_InsertSort(LinkList * list)
                 q = q->Next;
             }
         }
-        if(t != p)
+        if (t != p)
         {
             Element temp = p->Data;
             p->Data = t->Data;
@@ -458,20 +401,9 @@ void LinkList_InsertSort(LinkList * list)
     return;
 }
 
-//LinkList长度
-int GetSize(LinkList * list)
+//获取LinkList长度
+int GetSize_LinkList(LinkList * L)
 {
-    if (IsEmpty(list)) {
-        return 0;
-    }
-    return list->Count;
-
-    /*int ret = 0;
-    Node* p = list->Head->Next;
-    while (p)
-    {
-        ret++;
-        p = p->Next;
-    }
-    return ret;*/
+    if (IsEmptyLinkList(L)) return 0;
+    return L->Count;
 }
