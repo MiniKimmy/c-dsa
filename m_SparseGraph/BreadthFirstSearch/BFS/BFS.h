b4ac7986041ab4c49b64895f561750a8d4d43fc1
MinKKKKKK <343733617@qq.com>
2017-11-25 00:23:28 +0800

    完善了细节bfs内部方法

--- m_SparseGraph/BreadthFirstSearch/BFS/BFS.h
+++ m_SparseGraph/BreadthFirstSearch/BFS/BFS.h
@@ -19,13 +19,11 @@ typedef struct BFS {
 #include "queue.h"           //#include队列(作BFS绑定marked[]和from[])
 #include "stack.h"           //#include链栈(作GetPath用)
 
-
 #pragma region Functions
 BFS* InitBFS(Graph * G, int start);
-void BondingConnect_bfs(BFS * S, Graph * G, int start);
+void BondingConnect_bfs(BFS * S, Graph * G);
 Stack* GetPath(BFS * S, int destination);
 #pragma endregion
-
 
 /*初始化BFS*/
 /*G : 传入空数据的Graph指针*/
@@ -46,8 +44,8 @@ BFS* InitBFS(Graph * G, int start)
     CreateGraph_ByInputKeyCode(G);
 
     //再次检查图
-    if (start > GetVerticesCount(G)
-        || GetVerticesCount(G) == 0
+    if (start > GetVerticesCount(G) 
+        || GetVerticesCount(G) == 0 
         || GetEdgeCount(G) == 0) return NULL;
 
     BFS* ret = (BFS*)malloc(sizeof(BFS));
@@ -71,30 +69,30 @@ BFS* InitBFS(Graph * G, int start)
     //初始化marked[]全为FALSE,初始化from[]全为-1，-1只是标记该key未找到最近的上一个顶点
     for (int i = 0; i < GetVerticesCount(G); i++){
         ret->marked[i] = FALSE;
-        ret->from[i] = -1;
+        ret->from[i] = -1;  
     }
-
+    
     ret->start = start;        //记录start(GetPath时候需要用start)
     ret->marked[start] = TRUE; //自身和自身的连通的
     ret->from[start] = start;  //起点的上一个起点是自身
 
     //绑定连通marked[]和from[]
-    BondingConnect_bfs(ret, G, start);
+    BondingConnect_bfs(ret, G);
     return ret;
 }
 
-/*（内部方法）绑定连接*/
-void BondingConnect_bfs(BFS * S, Graph * G, int start)
+/*（内部方法）绑定连接构建marked[]和from[]*/
+void BondingConnect_bfs(BFS * S, Graph * G)
 {
     Queue* Q = InitQueue();
-    EnterQueue(Q, start);
+    EnterQueue(Q, S->start);
     while (!IsEmptyQueue(Q)){
         int key = *GetValue_Queue(Q);
         DeleteQueue(Q);
-        GNode* p = GetGNode(G, key);
+        GNode* p = GetGNode(G, key); 
         while (p->Next){
             p = p->Next;
-            if (S->from[p->Key] == -1 && p->Key != start) {
+            if (S->from[p->Key] == -1 && p->Key != S->start) {
                 EnterQueue(Q, p->Key);
                 S->from[p->Key] = key;
                 S->marked[p->Key] = TRUE;
@@ -108,7 +106,7 @@ void BondingConnect_bfs(BFS * S, Graph * G, int start)
 Stack* GetPath(BFS * S, int destination)
 {
     if (S->marked[destination] == FALSE) return;
-
+    
     Stack* ret = InitStack();
     for (int i = destination; i != S->start; i = S->from[i]){
         Push_Stack(ret, i);
