25164898f5e01310e17019ded8c6a164b4f6971c
MinKKKKKK <343733617@qq.com>
2018-01-04 18:13:43 +0800

    LinkList最终版.

--- Sequence/LinkList.h
+++ Sequence/LinkList.h
@@ -1,356 +1,319 @@
-#pragma once
 #include<stdio.h>
 #include<stdlib.h>
 #define PRINT_STRING(x) printf("%s\n",x)
 
-typedef enum Status{
+
+typedef int TElement;     //import到其他文件时候需要修改该类型
+
+
+/*Bool枚举*/
+typedef enum Status {
     FALSE,
     TRUE,
 }Status;
 
-typedef int Element;    //Include到其他文件要修改该Element类型
+/*LinkList类型枚举*/
+typedef enum LinkListType{
+    SINGLEWAY,
+    DOUBLEWAY,
+}LinkListType;
 
-#pragma region LinkList
-/*双哨兵+双向链表*/
+/*结点Node数据类型*/
 typedef struct Node {
-    Element Data;
-    struct Node * Next;
-    struct Node * Front;
+    TElement data;
+    struct Node* next;
+    struct Node* front;
 }Node;
 
 typedef struct LinkList {
-    struct Node * Head;
-    struct Node * Tail;
-    int Count;
+    int count;
+    Node* head;
+    Node* tail;
+    LinkListType type;
 }LinkList;
-#pragma endregion
 
-#pragma region Functions;
+#pragma region Functions
 LinkList* InitLinkList();
-void CreateLinkList_ByKeyCodeInput(LinkList * L);
-void CreateLinkList_ByInputArray(LinkList * L, int* arr, int size);
-Status IsEmptyLinkList(LinkList * L);
-void Append_LinkList(LinkList * L, Element value);
-void Insert_LinkList(LinkList * L, int index, Element value);
+Status IsEmpty_LinkList(LinkList * L);
 void Traverse_LinkList(LinkList * L);
-void DeleteNode_LinkList(LinkList * L, int index);
-void Remove_LinkList(LinkList * L);
-Element GetValue_LinkList(LinkList * L, int index);
-Element GetFirst_LinkList(LinkList * L);
-void SetValue_LinkList(LinkList * L, int index, Element value);
-void Destroy_LinkList(LinkList** L);
-void Clear_LinkList(LinkList * L);
-void Inversion_LinkList(LinkList * L);
+TElement* GetFirst_LinkList(LinkList * L);
+TElement* GetValue_LinkList(LinkList * L, int index);
+void SetValue_LinkList(LinkList * L, int index, TElement value);
+void Append_LinkList(LinkList * L, TElement value);
 int GetSize_LinkList(LinkList * L);
+void Inversion_LinkList(LinkList * L);
+void Insert_LinkList(LinkList * L, int index, TElement value);
+void Remove_LinkList(LinkList * L);
+void Delete_LinkList(LinkList * L, int index);
+void Clear_LinkList(LinkList * L);
+void Destroy_LinkList(LinkList** L);
 #pragma endregion
 
-/*初始化LinkList*/
+/*初始化LinkList.[默认双向LinkList]*/
 LinkList* InitLinkList()
 {
-    Node* m_Head = (Node*)malloc(sizeof(Node));
-    if (m_Head == NULL) {
-        PRINT_STRING("Node初始化动态分配内存失败");
+    //初始化头哨兵
+    Node* head = (Node*)malloc(sizeof(Node));
+    if (head == NULL) {
+        PRINT_STRING("node_head初始化动态分配内存失败");
         exit(-1);
     }
 
     LinkList* ret = (LinkList*)malloc(sizeof(LinkList));
     if (ret == NULL) {
-        printf("LinkList动态分配内存失败\n");
+        PRINT_STRING("LinkList动态分配内存失败");
         exit(-1);
     }
-    m_Head->Next = NULL;
-    m_Head->Front = NULL;
-    ret->Head = m_Head;
-    ret->Tail = m_Head;
-    ret->Count = 0;
+
+    head->next = NULL;
+    head->front = NULL;
+    ret->head = head;
+    ret->tail = head;
+    ret->count = 0;
+    ret->type = DOUBLEWAY;
     return ret;
 }
 
-//手动输入初始化LinkList
-void CreateLinkList_ByKeyCodeInput(LinkList * L)
-{
-    PRINT_STRING("请输入需要初始化元素的数量 :");
-    int size;
-    scanf_s("%d", &size);
-    while (size < 0){
-        PRINT_STRING("size不能为负值,请重新输入size的值：");
-        scanf_s("%d", &size);
-    }
-
-    for (int i = 0; i < size; i++) {
-        printf("请输入第%d个元素的value :", i);
-        Element arg;
-        scanf_s("%d", &arg);
-        Append_LinkList(L, arg);
-    }
-    return;
-}
-
-//判断LinkList是否为空
-Status IsEmptyLinkList(LinkList * L)
+/*判断LinkList是否为空*/
+Status IsEmpty_LinkList(LinkList * L)
 {
     if (L == NULL) {
-        PRINT_STRING("List为NULL\n");
-        exit(-1);
+        PRINT_STRING("LinkList为NULL");
+        return TRUE;
     }
 
-    if (L->Count == 0) return TRUE;
+    if (L->count == 0) return TRUE;
     else return FALSE;
-
-
 }
 
-//LinkList追加
-/*在LinkList的最后面添加新元素*/
-void Append_LinkList(LinkList * L, Element value)
-{
-    if (L == NULL) {
-        PRINT_STRING("List为NULL");
-        return;
-    }
-
-    Node* p = (Node*)malloc(sizeof(Node));
-    p->Data = value;
-    p->Next = NULL;
-    p->Front = L->Tail;
-    L->Tail->Next = p;
-    L->Tail = p;
-    L->Count++;
-    return;
-}
-
-//LinkList插入
-/*index：插入新元素的位置*/
-void Insert_LinkList(LinkList * L, int index, Element value)
-{
-    if (L == NULL) {
-        PRINT_STRING("L为NULL");
-        return;
-    }
-
-    if (index < 0 || index >= L->Count) {
-        printf("无法在第[%d]的位置插入元素\n", index);
-        return;
-    }
-
-    Node* p;
-    if (index > L->Count / 2) {
-        p = L->Tail;
-        for (int i = L->Count - 1; i >= index; i--) {
-            p = p->Front;
-        }
-    }else {
-        p = L->Head;
-        for (int i = 0; i < index; i++) {
-            p = p->Next;
-        }
-    }
-
-    Node* item = (Node*)malloc(sizeof(Node));
-    item->Data = value;
-    item->Next = p->Next;
-    item->Front = p;
-    p->Next->Front = item;
-    p->Next = item;
-    L->Count++;
-    return;
-}
-
-//通过数组赋值存储int类型Element的LinkList
-void CreateLinkList_ByInputArray(LinkList * L, int* arr, int size)
-{
-    if (arr == NULL || L == NULL) {
-        PRINT_STRING("传入参数有NULL");
-        return;
-    }
-
-    for (int i = 0; i < size; i++) {
-        Append_LinkList(L, arr[i]);
-    }
-    return;
-}
-
-//遍历LinkList
+/*遍历LinkList*/
 void Traverse_LinkList(LinkList * L)
 {
-    if (IsEmptyLinkList(L)) return;
+    if (IsEmpty_LinkList(L)) return;
 
-    Node* p = L->Head->Next;
-    while (p) {
-        printf("%d\n", p->Data);
-        p = p->Next;
+    Node* node = L->head->next;
+    for (int i = 0; i < L->count; i++){
+        printf("%d ",node->data);
+        node = node->next;
     }
-    return;
+    PRINT_STRING("");
 }
 
-//获取指定index的value
-Element GetValue_LinkList(LinkList * L, int index)
+/*返回LinkList第一个元素指针*/
+TElement* GetFirst_LinkList(LinkList * L)
 {
-    if (IsEmptyLinkList(L)) return NULL;
+    if (L->head->next == NULL) return NULL;
+    return &(L->head->next->data);
+}
 
-    if (index < 0 || index >= L->Count) {
-        printf("无法获取第[%d]个位置的元素\n", index);
+/*返回LinkList该index的元素指针*/
+/*index:所获取元素的下标*/
+TElement* GetValue_LinkList(LinkList * L, int index)
+{
+    if (IsEmpty_LinkList(L)) return NULL;
+
+    if (index >= L->count || index < 0) {
+        printf("无法在第[%d]的位置GetValue\n", index);
         return NULL;
     }
 
-    Node* p;
-    if (index > L->Count / 2) {
-        p = L->Tail;
-        for (int i = L->Count - 1; i > index; i--) {
-            p = p->Front;
-        }
-    }
-    else {
-        p = L->Head;
-        for (int i = 0; i <= index; i++) {
-            p = p->Next;
-        }
-    }
-    return p->Data;
-}
-
-//获取第一个元素的value
-Element GetFirst_LinkList(LinkList * L)
-{
-    if (IsEmptyLinkList(L)) return NULL;
-    return L->Head->Next->Data;
-}
-
-//修改指定index的value
-void SetValue_LinkList(LinkList * L, int index, Element value)
-{
-    if (IsEmptyLinkList(L)) return;
-    if (index < 0 || index >= L->Count) {
-        printf("无法修改第[%d]个位置的元素value\n", index);
-        return;
-    }
-
-    Node* p;
-    if (index > L->Count / 2) {
-        p = L->Tail;
-        for (int i = L->Count - 1; i > index; i--) {
-            p = p->Front;
-        }
-    }
-    else {
-        p = L->Head;
-        for (int i = 0; i <= index; i++) {
-            p = p->Next;
-        }
-    }
-    p->Data = value;
-    return;
-}
-
-//删除指定index的value
-void DeleteNode_LinkList(LinkList * L, int index)
-{
-    if (IsEmptyLinkList(L)) return;
-
-    if (index < 0 || index >= L->Count) {
-        printf("无法删除第[%d]个位置的元素\n", index);
-        return;
-    }
-
-    Node* p;
-    if (index > L->Count / 2) {
-        p = L->Tail;
-        for (int i = L->Count - 1; i >= index; i--) {
-            p = p->Front;
-        }
+    Node* ret = NULL;
+    if (L->type == DOUBLEWAY && index >= L->count / 2) {
+        ret = L->tail;
+        int pos = L->count - 1 - index;
+        while (pos-- > 0) ret = ret->front;
     }else {
-        p = L->Head;
-        for (int i = 0; i < index; i++) {
-            p = p->Next;
-        }
+        ret = L->head->next;
+        int pos = 0;
+        while (pos++ < index) ret = ret->next;
     }
 
-    Node* temp = p->Next;
-    temp->Next->Front = p;
-    p->Next = temp->Next;
-    free(temp);
-    temp->Next = NULL;
-    temp->Front = NULL;
-    L->Count--;
+    return ret == NULL? NULL : &(ret->data);
+}
+
+/*追加LinkList*/
+void Append_LinkList(LinkList * L, TElement value)
+{
+    if (L == NULL) return;
+    Node* node = (Node*)malloc(sizeof(Node));
+    if (node == NULL) {
+        PRINT_STRING("追加node动态分配内存失败");
+        exit(-1);
+    }
+
+    node->data = value;
+    node->front = L->tail;
+    node->next = NULL;
+    L->tail->next = node;
+    L->tail = node;
+    L->count++;
     return;
 }
 
-//删除最后一个value
+/*修改LinkList该index的元素value*/
+void SetValue_LinkList(LinkList * L, int index, TElement value)
+{
+    if (IsEmpty_LinkList(L)) return;
+
+    if (index >= L->count || index < 0) {
+        printf("无法在第[%d]的位置SetValue\n", index);
+        return;
+    }
+
+    Node* ret = NULL;
+    if (L->type == DOUBLEWAY && index >= L->count / 2) {
+        ret = L->tail;
+        int pos = L->count - 1 - index;
+        while (pos-- > 0) ret = ret->front;
+    }else {
+        ret = L->head->next;
+        int pos = 0;
+        while (pos++ < index) ret = ret->next;
+    }
+
+    ret->data = value;
+    return;
+}
+
+/*返回LinkList当前元素个数*/
+int GetSize_LinkList(LinkList * L)
+{
+    if (L == NULL) {
+        PRINT_STRING("LinkList为NULL");
+        return 0;
+    }
+    return L->count;
+}
+
+/*倒置LinkList*/
+void Inversion_LinkList(LinkList * L)
+{
+    if (IsEmpty_LinkList(L)) return;
+
+    Node* h_node = L->head->next;
+    Node* t_node = L->tail;
+    for (int i = 0; i < L->count/2 && h_node != t_node; i++){
+        TElement temp = h_node->data;
+        h_node->data = t_node->data;
+        t_node->data = temp;
+        h_node = h_node->next;
+        t_node = t_node->front;
+    }
+    return;
+}
+
+/*插入元素LinkList*/
+void Insert_LinkList(LinkList * L, int index, TElement value)
+{
+    if (L == NULL) return;
+
+    if (index >= L->count || index < 0) {
+        printf("无法在第[%d]的位置Insert\n", index);
+        return;
+    }
+
+    Node* node = (Node*)malloc(sizeof(Node));
+    if (node == NULL) {
+        PRINT_STRING("插入node动态分配内存失败");
+        exit(-1);
+    }
+
+    Node* p = NULL;
+    if (L->type == DOUBLEWAY && index >= L->count / 2) {
+        p = L->tail;
+        int pos = L->count - 1 - index;
+        while (pos-- > 0) p = p->front;
+    }else {
+        p = L->head->next;
+        int pos = 0;
+        while (pos++ < index) p = p->next;
+    }
+
+    node->data = value;
+    node->next = p;
+    node->front = p->front;
+    p->front->next = node;
+    p->front = node;
+    L->count++;
+    return;
+}
+
+/*删除LinkList第一个元素*/
 void Remove_LinkList(LinkList * L)
 {
-    if (IsEmptyLinkList(L)) return;
+    if (IsEmpty_LinkList(L)) return;
+    Node* node = L->head->next;
 
-    Node * p = L->Tail;
-    L->Tail = p->Front;
-    free(p);
-    p->Front = NULL;
-    L->Tail->Next = NULL;
-    L->Count--;
+    if (node->next == NULL) {
+        L->tail = node->front;
+    }else {
+        node->next->front = node->front;
+    }
+    node->front->next = node->next;
+
+    node->front = NULL;
+    node->next = NULL;
+    free(node);
+    L->count--;
     return;
 }
 
-/*清除LinkList ,返回一个空元素的LinkList*/
+/*删除LInkList该index的元素*/
+/*index:要删除元素的下标*/
+void Delete_LinkList(LinkList * L, int index)
+{
+    if (L == NULL) return;
+
+    if (index >= L->count || index < 0) {
+        printf("无法在第[%d]的位置Delete\n", index);
+        return;
+    }
+
+    Node* p = NULL;
+    if (L->type == DOUBLEWAY && index >= L->count / 2) {
+        p = L->tail;
+        int pos = L->count - 1 - index;
+        while (pos-- > 0) p = p->front;
+    }else {
+        p = L->head->next;
+        int pos = 0;
+        while (pos++ < index) p = p->next;
+    }
+
+    if (p->next == NULL) {
+        L->tail = p->front;
+    }else {
+        p->next->front = p->front;
+    }
+    p->front->next = p->next;
+
+    p->front = NULL;
+    p->next = NULL;
+    free(p);
+    L->count--;
+    return;
+}
+
+/*清空LinkList*/
+/*[头哨兵并未free掉]*/
 void Clear_LinkList(LinkList * L)
 {
-    if (IsEmptyLinkList(L)) return;
+    if (L == NULL) return;
 
-    Node* p = L->Head->Next;
-    while (p) {
-        Node* q = p->Next;
-        free(p);
-        p->Front = NULL;
-        p->Next = NULL;
-        p = q;
+    int count = L->count;
+    for (int i = 0; i < count; i++){
+        Remove_LinkList(L);
     }
-    L->Tail = L->Head;
-    L->Head->Next = NULL;
-    L->Head->Front = NULL;
-    L->Count = 0;
     return;
 }
 
-/*销毁LinkList,返回NULL*/
+/*销毁LinkList*/
 void Destroy_LinkList(LinkList** L)
 {
     if (*L == NULL) return;
-
-    Node* p = (*L)->Head;
-    while (p) {
-        Node* q = p->Next;
-        free(p);
-        p->Front = NULL;
-        p->Next = NULL;
-        p = q;
-    }
-
+    Clear_LinkList(*L);
+    free(((*L)->head));
     free(*L);
-    (*L)->Tail = NULL;
-    (*L)->Head = NULL;
     *L = NULL;
     return;
-}
-
-//LinkList倒置
-void Inversion_LinkList(LinkList * L)
-{
-    if (IsEmptyLinkList(L)) return;
-
-    Node* p = L->Head->Next;
-    Node* q = L->Tail;
-    for (int i = 0; i < L->Count / 2; i++)
-    {
-        int temp = p->Data;
-        p->Data = q->Data;
-        q->Data = temp;
-        p = p->Next;
-        q = q->Front;
-    }
-    return;
-}
-
-//获取LinkList长度
-int GetSize_LinkList(LinkList * L)
-{
-    if (IsEmptyLinkList(L)) return 0;
-    return L->Count;
 }
