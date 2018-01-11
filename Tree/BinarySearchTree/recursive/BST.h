3e3b719a123bc739f274d0ccd8e79ff30852344d
MinKKKKKK <343733617@qq.com>
2018-01-11 16:36:33 +0800

    最终版BST[递归实现删除].比之前的更好理解

--- Tree/BinarySearchTree/recursive/BST.h
+++ Tree/BinarySearchTree/recursive/BST.h
@@ -1,335 +1,301 @@
-#pragma once
 #include<stdio.h>
 #include<stdlib.h>
-#define PRINT_INT(x) printf("%d\n",x);
-#define PRINT_KEY_VALUE(x,y) printf("{KEY = %d, VALUE = %d}\n",x,y)
+#define PRINT_STRING(x) printf("%s\n",x)
 
-typedef int KEY;
-typedef int Element;
+typedef int BSTData;  //import到其他文件时候需要修改该类型,Destroy/Delete等方法是否需要free该BSTData
+typedef int KEY;      //import到其他文件时候需要修改该类型,Destroy/Delete等方法是否需要free该KEY
 
-#pragma region BST
-typedef struct BinarySearchNode {
-    KEY Key;
-    Element Data;
-    struct BinarySearchNode *Left;
-    struct BinarySearchNode *Right;
-}BSNode, *BST;
-#pragma endregion
+typedef struct BSTNode {
+    KEY key;
+    BSTData data;
+    struct BSTNode* lchild;
+    struct BSTNode* rchild;
+}BST;
 
 #pragma region Functions
-#pragma region PRIVATEFunctions
-    BST DeleteMin(BST * T);
-    BST DeleteMax(BST * T);
-    BST Delete(BST * T, KEY key);
-#pragma endregion
-BST InitBSTree();
-void Insert(BST * T, KEY key, Element value);
-Element* Search(BST T, KEY key);
-BSNode* GetMin(BST T);
-BSNode* GetMax(BST T);
-void DeleteNode(BST * T, KEY key);
-void DeleteMinNode(BST * T);
-void DeleteMaxNode(BST * T);
-void DestroyBSTree(BST * T);
-int GetDepth(BST T);
+BST* InitBST();
+void Insert_BST(BST** T, KEY key, BSTData value);
+BST* Search_BST(BST* T, KEY key);
+BST* GetMin_BST(BST* T);
+BST* GetMax_BST(BST* T);
+void DeleteMin_BST(BST** T);
+void DeleteMax_BST(BST** T);
+void Delete_BST(BST** T, KEY key);
+void Destroy_BST(BST** T);
+int GetDepth_BST(BST* T);
 #pragma region Traverse
-    void InOrderTraverse(BST T);
-    void PreOrderTraverse(BST T);
-    void PostOrderTraverse(BST T);
-    void LevelOrderTraverse(BST T);
+    void InOrderTraverse(BST* T);
+    void PreOrderTraverse(BST* T);
+    void PostOrderTraverse(BST* T);
+    void LevelOrderTraverse(BST* T);
 #pragma endregion
 #pragma endregion
 
-#include "queue.h"    //在BST定义之后再Include队列
+#include "queue.h"   //在BST定义之后再include
 
 
-/*BST初始化*/
-BST InitBSTree()
+/*初始化BST*/
+BST* InitBST()
 {
     return NULL;
 }
 
-/*通过手动赋值创建一棵搜索二叉树*/
-void CreateBSTree_ByInputKeyCode(BST * T)
+/*插入新的树node*/
+/*key:插入的键*/
+/*value:该键对应的数据*/
+/*若key与之前相同则覆盖value*/
+void Insert_BST(BST** T, KEY key, BSTData value)
 {
-    printf("Key为-1则NULL,请输入添加的KEY值：");
-    KEY m_Key;
-    scanf_s("%d", &m_Key);
-    if (m_Key != -1) {
-        printf("请输入该[%d]元素对应的value值：", m_Key);
-        Element m_value;
-        scanf_s("%d", &m_value);
-
-        while (m_Key != -1)
-        {
-            Insert(T, m_Key, m_value);
-            printf("Key为-1则NULL,请输入添加的KEY值：");
-            scanf_s("%d", &m_Key);
-            if (m_Key != -1) {
-                printf("请输入该[%d]元素对应的value值：", m_Key);
-                scanf_s("%d", &m_value);
-            }
+    //如果树node为NULL
+    if ((*T) == NULL) {
+        (*T) = (BST*)malloc(sizeof(BST));
+        if ((*T) == NULL) {
+            PRINT_STRING("BSTnode初始化动态分配内存失败");
         }
+
+        (*T)->key = key;
+        (*T)->data = value;
+        (*T)->lchild = NULL;
+        (*T)->rchild = NULL;
+        return;
     }
-    return;
+
+    //如果树node不为NULL
+    if ((*T)->key == key) {
+        (*T)->data = value;
+        return;
+    }else if ((*T)->key < key) {
+        Insert_BST(&((*T)->rchild), key, value);
+    }else {
+        Insert_BST(&((*T)->lchild), key, value);
+    }
 }
 
-/*插入新元素,若KEY值与之前重复，直接替换value*/
-/*T：插入的位置，key：设置的KEY值，value：实际的Data值*/
-void Insert(BST * T, KEY key, Element value)
+/*根据key搜索树node*/
+BST* Search_BST(BST* T, KEY key)
 {
-    if (*T == NULL) {
-        *T = (BST)malloc(sizeof(BSNode));
-        if (*T == NULL) {
-            printf("BST初始化动态分配内存失败\n");
-            exit(-1);
-        }
-        (*T)->Key = key;
-        (*T)->Data = value;
-        (*T)->Left = NULL;
-        (*T)->Right = NULL;
+    if (T == NULL) return NULL;
+
+    if (T->key == key) return T;
+    else if (T->key > key) {
+        return Search_BST(T->lchild, key);
+    }else {
+        return Search_BST(T->rchild, key);
     }
+}
+
+/*获取最小的key的树node*/
+BST* GetMin_BST(BST* T)
+{
+    if (T == NULL) return NULL;
+    else if (T->lchild != NULL) return GetMin_BST(T->lchild);
+    else return T;
+}
+
+/*获取最大的key的树node*/
+BST* GetMax_BST(BST* T)
+{
+    if (T == NULL) return NULL;
+    else if (T->rchild != NULL) return GetMax_BST(T->rchild);
+    else return T;
+}
+
+/*删除最小的key的树node*/
+void DeleteMin_BST(BST** T)
+{
+    if ((*T) == NULL) return;
+    else if ((*T)->lchild != NULL && ((*T)->lchild && (*T)->lchild->lchild != NULL ) ) return DeleteMin_BST(&((*T)->lchild));
     else {
-        if (key == (*T)->Key) {
-            (*T)->Data = value;
-            return;
+        //如果是树root
+        if ((*T)->lchild == NULL){
+            BST* temp = *T;
+            *T = (*T)->rchild;
+            //free(temp->key)
+            //free(temp->data)
+            temp->rchild = NULL;
+            free(temp);
+            temp = NULL;
+        }else {
+            BST* temp = (*T)->lchild;
+            (*T)->lchild = temp->rchild;
+            //free(temp->key)
+            //free(temp->data)
+            free(temp);
+            temp = NULL;
         }
-        if (key < (*T)->Key) {
-            Insert(&((*T)->Left), key, value);
+    return;
+    }
+}
+
+/*删除最大的key的树node*/
+void DeleteMax_BST(BST** T)
+{
+    if ((*T) == NULL) return;
+    else if ((*T)->rchild != NULL && ((*T)->rchild && (*T)->rchild->rchild != NULL)) return DeleteMax_BST(&((*T)->rchild));
+    else {
+        //如果是树root
+        if ((*T)->rchild == NULL) {
+            BST* temp = *T;
+            *T = (*T)->lchild;
+            //free(temp->key)
+            //free(temp->data)
+            temp->lchild = NULL;
+            free(temp);
+            temp = NULL;
         }
         else {
-            Insert(&((*T)->Right), key, value);
+            BST* temp = (*T)->rchild;
+            (*T)->rchild = temp->lchild;
+            //free(temp->key)
+            //free(temp->data)
+            free(temp);
+            temp = NULL;
         }
-    }
-    return;
-}
-
-/*根据Key找到对应的value*/
-Element* Search(BST T, KEY key)
-{
-    if (T == NULL) {
-        printf("无法操作空BST\n");
-        return    NULL;
-    }
-
-    if (T->Key == key) {
-        return &(T->Data);
-    }
-    if (T->Key > key) {
-        return Search(T->Left, key);
-    }
-    else {
-        return Search(T->Right, key);
-    }
-}
-
-/*返回最左下角的Node,Min是指最小的KEY值*/
-BSNode* GetMin(BST T)
-{
-    if (T == NULL) {
-        printf("无法操作空BST\n");
-        return    NULL;
-    }
-
-    if (T->Left != NULL) {
-        return GetMin(T->Left);
-    }else if (T->Left == NULL) {
-        return T;
-    }
-}
-
-/*返回最右下角的Node,Max是指最大的KEY值*/
-BSNode* GetMax(BST T)
-{
-    if (T == NULL) {
-        printf("无法操作空BST\n");
-        return    NULL;
-    }
-
-    if (T->Right != NULL) {
-        return GetMax(T->Right);
-    }else if (T->Right == NULL) {
-        return T;
-    }
-}
-
-/*(内部方法)删除KEY最小的Node*/
-BST DeleteMin(BST * T)
-{
-    if (*T == NULL) {
-        printf("无法操作空BST\n");
         return;
     }
-
-    if ((*T)->Left != NULL) {
-        (*T)->Left = DeleteMin(&(*T)->Left);
-    }else if ((*T)->Left == NULL) {
-        BST ret = (*T)->Right;
-        free(*T);
-        (*T)->Left = NULL;
-        return ret;
-    }
 }
 
-/*(内部方法)删除KEY最大的Node*/
-BST DeleteMax(BST * T)
+/*删除指定key的树node*/
+void Delete_BST(BST** T, KEY key)
 {
-    if (*T == NULL) {
-        printf("无法操作空BST\n");
-        return;
-    }
-
-    if ((*T)->Right != NULL) {
-        (*T)->Right = DeleteMax(&(*T)->Right);
-    }else if ((*T)->Right == NULL) {
-        BST ret = (*T)->Left;
-        free(*T);
-        (*T)->Right = NULL;
-        return ret;
-    }
-}
-
-/*(内部方法)删除指定KEY的Node[默认优先把右Child拉上去]*/
-BST Delete(BST * T, KEY key)
-{
-    if (*T == NULL) return NULL;
-    if ((*T)->Key > key) {
-        (*T)->Left = Delete(&((*T)->Left), key);
-    }
-    else if ((*T)->Key < key) {
-        (*T)->Right = Delete(&((*T)->Right), key);
+    if ((*T) == NULL) return;
+    if ((*T)->key > key) {
+        Delete_BST(&((*T)->lchild), key);
+    }else if((*T)->key < key) {
+        Delete_BST(&((*T)->rchild), key);
     }
     else {
-        if ((*T)->Right) {
-            BST p = GetMin((*T)->Right);
-            if ((*T)->Right == p) {
-                p->Left = (*T)->Left;
-                return p;
-            }
-            BST Min_Copy = (BST)malloc(sizeof(BSNode));
-            if (Min_Copy == NULL) {
-                printf("Min_Copy初始化动态分配内存失败\n");
+        //如果有RChild,就用RChild的Min交换，
+        if ((*T)->rchild) {
+            BST* r_Min = (BST*)malloc(sizeof(BST));
+            if (r_Min == NULL) {
+                PRINT_STRING("Delete方法的r_Min动态分配内存失败");
                 exit(-1);
             }
-            Min_Copy->Data = p->Data;
-            Min_Copy->Key = p->Key;
-            Min_Copy->Left = (*T)->Left;
-            Min_Copy->Right = Delete(&((*T)->Right), p->Key);
-            free(*T);
-            *T = NULL;
-            return Min_Copy;
+
+            //把RChild的Min先Copy再free
+            BST* Copy_min = GetMin_BST((*T)->rchild);
+            r_Min->key = Copy_min->key;
+            r_Min->data = Copy_min->data;
+            r_Min->lchild = NULL;
+            r_Min->rchild = NULL;
+
+            //二次递归free掉Min, Copy_min = NULL可以不写
+            Delete_BST(T, Copy_min->key);
+            Copy_min = NULL;
+
+            (*T)->key = r_Min->key;
+            (*T)->data = r_Min->data;
         }
-        else if ((*T)->Left) {
-            BST ret = (*T)->Left;
-            free(*T);
-            *T = NULL;
-            return ret;
+
+
+        //如果没有RChild就用LChind的Max交换
+        else if ((*T)->lchild){
+            BST* l_Max = (BST*)malloc(sizeof(BST));
+            if (l_Max == NULL) {
+                PRINT_STRING("Delete方法的l_Max动态分配内存失败");
+                exit(-1);
+            }
+
+            //把LChild的Man先Copy再free
+            BST* Copy_max = GetMax_BST((*T)->lchild);
+            l_Max->key = Copy_max->key;
+            l_Max->data = Copy_max->data;
+            l_Max->lchild = NULL;
+            l_Max->rchild = NULL;
+
+            //二次递归free掉Man, Copy_max = NULL可以不写
+            Delete_BST(T, Copy_max->key);
+            Copy_max = NULL;
+
+            (*T)->key = l_Max->key;
+            (*T)->data = l_Max->data;
         }
+
+        //如果L/RChild都没有,就直接free掉
         else {
             free(*T);
             *T = NULL;
-            return NULL;
         }
     }
-    return *T;
-}
-
-/*(外部方法)DeleteMin*/
-void DeleteMinNode(BST * T) 
-{
-    *T = DeleteMin(T);
-    return;
-}
-
-/*(外部方法)DeleteMin*/
-void DeleteMaxNode(BST * T)
-{
-    *T = DeleteMax(T);
-    return;
-}
-
-/*(外部方法)Delete*/
-void DeleteNode(BST * T, KEY key)
-{
-    *T = Delete(T, key);
-    return;
 }
 
 /*销毁BST*/
-void DestroyBSTree(BST * T)
+void Destroy_BST(BST** T)
 {
-    if (*T)
-    {
-        DestroyBSTree(&((*T)->Left));
-        DestroyBSTree(&((*T)->Right));
+    if (*T != NULL) {
+        Destroy_BST(&((*T)->lchild));
+        Destroy_BST(&((*T)->rchild));
+        //free((*T)->data)
+        //free((*T)->key)
         free(*T);
         *T = NULL;
     }
+    return;
 }
 
-/*树的深度*/
-int GetDepth(BST T)
+/*前序Traverse*/
+void PreOrderTraverse(BST* T)
 {
-    int i, j;
+    if (T != NULL)
+    {
+        printf("key = %d data = %d\n", T->key, T->data);
+        PreOrderTraverse(T->lchild);
+        PreOrderTraverse(T->rchild);
+    }
+    return;
+}
+
+/*中序Traverse*/
+void InOrderTraverse(BST* T)
+{
+    if (T != NULL)
+    {
+        PreOrderTraverse(T->lchild);
+        printf("key = %d data = %d\n", T->key, T->data);
+        PreOrderTraverse(T->rchild);
+    }
+    return;
+}
+
+/*后序Traverse*/
+void PostOrderTraverse(BST* T)
+{
+    if (T != NULL)
+    {
+        PreOrderTraverse(T->lchild);
+        PreOrderTraverse(T->rchild);
+        printf("key = %d data = %d\n", T->key, T->data);
+    }
+    return;
+}
+
+/*层序Traverse*/
+void LevelOrderTraverse(BST* T)
+{
+    if (T == NULL) return;
+
+    Queue* Q = InitQueue();
+    En_Queue(Q, T);
+    while (!IsEmpty_Queue(Q)) {
+        BST* temp = *GetValue_Queue(Q);
+        De_Queue(Q);
+        printf("key = %d data = %d\n", temp->key,temp->data);
+        if (temp->lchild) En_Queue(Q, temp->lchild);
+        if (temp->rchild) En_Queue(Q, temp->rchild);
+    }
+    Destroy_Queue(&Q);
+    return;
+}
+
+/*获取树的深度*/
+int GetDepth_BST(BST* T)
+{
     if (T == NULL) return 0;
-    i = GetDepth(T->Left);
-    j = GetDepth(T->Right);
+
+    int i = 0;
+    int j = 0;
+    i = GetDepth_BST(T->lchild);
+    j = GetDepth_BST(T->rchild);
     return i > j ? i + 1 : j + 1;
 }
-
-#pragma region Traverse
-/*前序遍历*/
-void PreOrderTraverse(BST T)
-{
-    if (T)
-    {
-        PRINT_KEY_VALUE(T->Key, T->Data);
-        PreOrderTraverse(T->Left);
-        PreOrderTraverse(T->Right);
-    }
-    return;
-}
-
-/*中序遍历*/
-void InOrderTraverse(BST T)
-{
-    if (T)
-    {
-        InOrderTraverse(T->Left);
-        PRINT_KEY_VALUE(T->Key, T->Data);
-        InOrderTraverse(T->Right);
-    }
-    return;
-}
-
-/*后序遍历*/
-void PostOrderTraverse(BST T)
-{
-    if (T)
-    {
-        PostOrderTraverse(T->Left);
-        PostOrderTraverse(T->Right);
-        PRINT_KEY_VALUE(T->Key, T->Data);
-    }
-    return;
-}
-
-/*层序遍历*/
-void LevelOrderTraverse(BST T)
-{
-    Queue* q = InitQueue();
-    if (T)
-    {
-        EnterQueue(q, T);
-        while (!IsEmptyQueue(q))
-        {
-            TElement result = *GetHeadValue(q);
-            DeleteQueue(q);
-            PRINT_KEY_VALUE(result->Key, result->Data);
-            if (result->Left)  EnterQueue(q, result->Left);
-            if (result->Right) EnterQueue(q, result->Right);
-        }
-    }
-    DestroyQueue(&q);
-}
-#pragma endregion
